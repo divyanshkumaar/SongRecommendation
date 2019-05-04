5c3b464e09d331d5681bfee553e480b5bf989a0e
Divyansh Kumar <33126997+divyanshkumaar@users.noreply.github.com>
2019-05-04 13:12:12 +0530

    Add files via upload

---
+++ recomforyou.py
@@ -0,0 +1,12 @@
+import song
+print("----------------------------------------Welcome to Recommendation for You--------------------------------------\n")
+opt= input(print("Enter 1 if you want Movie Recommendations\nEnter 2 if you want Song Recommendations\n"))
+if opt == 1:
+	movie=int(input(print("Enter User ID:\n")))
+elif opt == 2:
+	songs=int(input(print("Enter User ID:\n")))
+	print(train_data.head(songs))
+	user_id = users[songs]
+	print(pm.recommend(user_id))
+else: 
+	print("Please Enter a Valid Choice.")
